1438b25629ce19bf79c922de16adfb350629aa41
Patrick Ware <patrick.r.ware@gmail.com>
2020-03-22 21:59:07 -0700

    save to revert

--- build.py
+++ build.py
@@ -29,12 +29,13 @@ template = open('./templates/base.html').read()
 
 view = ".view {{ height:50%;}}"
 
+<<<<<<< HEAD
 #insert CSS into template if page only displays half of background image
+=======
+>>>>>>> parent of 9bba7fc... main() for loop debugging, page_view() works with return variable
 def page_view():
 	view_template = template.replace("{{view}}", view)
 	return view_template
-
-view_template = page_view()
 
 def main():
 	print("Building static site")
@@ -43,16 +44,17 @@ def main():
 
 	for page in pages:
 		if page['image_display'] == 'half':
+			page_view()
 			page_filename = page['filename']
 			page_content = open(page_filename).read()
 			page_output = page['output']
-			combined_page = view_template.replace("{{content}}", page_content)
+			combined_page = template.reaplce("{{view}}", view
 			open(page_output, 'w+').write(combined_page)
 		else:
 			page_filename = page['filename']
 			page_content = open(page_filename).read()
 			page_output = page['output']
-			combined_page = template.replace("{{content}}", page_content)
+			combined_page = top_html + page_content + bottom_html
 			open(page_output, 'w+').write(combined_page)
 
 
