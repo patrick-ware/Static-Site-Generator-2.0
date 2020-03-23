7d086bd66b2ebd3a5e8733b7346c24adde39f3d5
Patrick Ware <patrick.r.ware@gmail.com>
2020-03-22 18:02:43 -0700

    view function and vairables added, not sure if page_view() works

--- build.py
+++ build.py
@@ -1,15 +1,32 @@
+template = open('./templates/base.html').read()
+view = ".view {{ height:50%;}}"
+
+def page_view():
+	view_template = template.replace("{{view}}", view)
+	return view_template
+
 def main():
 	print("Building static site")
 
 	top_html = open('./templates/top.html').read()
 	bottom_html = open('./templates/bottom.html').read()
 
+	template = open('./templates/base.html').read()
+
 	for page in pages:
-		page_filename = page['filename']
-		page_content = open(page_filename).read()
-		page_output = page['output']
-		combined_page = top_html + page_content + bottom_html
-		open(page_output, 'w+').write(combined_page)
+		if page['image_display'] == 'half':
+			page_view()
+			page_filename = page['filename']
+			page_content = open(page_filename).read()
+			page_output = page['output']
+			combined_page = template.reaplce("{{view}}", view
+			open(page_output, 'w+').write(combined_page)
+		else:
+			page_filename = page['filename']
+			page_content = open(page_filename).read()
+			page_output = page['output']
+			combined_page = top_html + page_content + bottom_html
+			open(page_output, 'w+').write(combined_page)
 
 
 #'display' item used to inform how much of background image is shown on each page
@@ -19,29 +36,28 @@ pages = [
 		'filename': './content/index.html',
 		'output': './docs/index.html',
 		'title': 'Patrick Ware',
-		'display': 'full'
+		'image_display': 'full'
 	},
 	{
 		'filename': './content/aboutme.html',
 		'output': './docs/aboutme.html',
 		'title': 'About Me',
-		'display': 'half'
+		'image_display': 'half'
 	},
 	{
 		'filename': './content/resume.html',
 		'output': './docs/resume.html',
 		'title': 'Resume',
-		'display': 'half'
+		'image_display': 'half'
 	},
 	{
 		'filename':'./content/contact.html',
 		'output': './docs/contact.html',
 		'title':'Contact',
-		'display': 'full'
+		'image_display': 'full'
 	}
 ]
 
 if __name__ == "__main__":
 	main()
-
 
