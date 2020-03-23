dc3873971784b9f0458c48c0c3487b15a9799a04
Patrick Ware <patrick.r.ware@gmail.com>
2020-03-22 21:52:57 -0700

    variables moved to beginning of script

--- build.py
+++ build.py
@@ -1,7 +1,35 @@
 template = open('./templates/base.html').read()
+
 view = ".view {{ height:50%;}}"
 
-#function to insert CSS into template if page only displays half of background image
+pages = [	
+	{
+		'filename': './content/index.html',
+		'output': './docs/index.html',
+		'title': 'Patrick Ware',
+		'image_display': 'full'
+	},
+	{
+		'filename': './content/aboutme.html',
+		'output': './docs/aboutme.html',
+		'title': 'About Me',
+		'image_display': 'half'
+	},
+	{
+		'filename': './content/resume.html',
+		'output': './docs/resume.html',
+		'title': 'Resume',
+		'image_display': 'half'
+	},
+	{
+		'filename':'./content/contact.html',
+		'output': './docs/contact.html',
+		'title':'Contact',
+		'image_display': 'full'
+	}
+]
+
+#insert CSS into template if page only displays half of background image
 def page_view():
 	view_template = template.replace("{{view}}", view)
 	return view_template
@@ -10,9 +38,6 @@ view_template = page_view()
 
 def main():
 	print("Building static site")
-
-	top_html = open('./templates/top.html').read()
-	bottom_html = open('./templates/bottom.html').read()
 
 	template = open('./templates/base.html').read()
 
