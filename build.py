39dae06fab288d1f12b1b84a8a9d778a2980173c
Patrick Ware <patrick.r.ware@gmail.com>
2020-03-29 12:37:30 -0700

    second function broken down into two separate functions

--- build.py
+++ build.py
@@ -1,28 +1,42 @@
 def main():
 	print("Building site...")
 	for page in pages:
-		generate_page(page)
+		insert_header(page)
+		insert_content(page)
 		write_data(page)
 	print("Site built")
 
 
-#Conditional statement used to correctly place content and modify background image based on image_display value, and insert title
-def generate_page(webpage):
-	filename = webpage['filename']
+#Conditional statement used to insert title and add active page marker to appropriate pages
+def insert_header(webpage):
+	title = webpage['title']
 	template = open('./templates/base.html').read()	
-	content = open(filename).read()
-	image_display = webpage['image_display']
-	if image_display == 'half':
-		combined_page = template.replace(webpage['title'], '>>'+webpage['title']).replace('{{title}}', webpage['title']).replace('{{view}}', '50%').replace('{{content_halfpage}}', content).replace('{{content_fullpage}}','')
+
+	if title == 'Patrick Ware':
+		custom_template = template.replace('{{title}}', webpage['title'])
 	else:
-		combined_page = template.replace(webpage['title'], '>>'+webpage['title']).replace('{{title}}', webpage['title']).replace('{{view}}', '100%').replace('{{content_fullpage}}', content).replace('{{content_halfpage}}','')
+		custom_template = template.replace(webpage['title'], '>>'+webpage['title']).replace('{{title}}', webpage['title'])
+	
+	return custom_template
+
+#Conditional statement used to correctly place content and modify pageground image based on image_display_value
+def insert_content(webpage):
+	filename = webpage['filename']
+	image_display = webpage['image_display']
+	custom_template = insert_header(webpage)
+	content = open(filename).read()
+
+	if image_display == 'half':
+		combined_page = custom_template.replace('{{view}}', '50%').replace('{{content_halfpage}}', content).replace('{{content_fullpage}}','')
+	else:
+		combined_page = custom_template.replace('{{view}}', '100%').replace('{{content_fullpage}}', content).replace('{{content_halfpage}}','')
 	
 	return combined_page
 
 #Combined_page value data passed to write_data function to write file to disk
 def write_data(webpage):
 	output = webpage['output']
-	combined_page = generate_page(webpage)
+	combined_page = insert_content(webpage)
 	open(output, 'w+').write(combined_page)
 
 pages = [	
